45398e96b59612f7ccbb6584cc89a67324b0aafc
RobRoch <rochon18@gmail.com>
2017-04-21 20:30:19 +0200

    Input works, and rerenders. BIG performance issue!

--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit} from '@angular/core';
-
+import { Router } from '@angular/router'
 //Called ActivatedRoute to access my movie cards' id.
 import { ActivatedRoute } from '@angular/router'
 import { MovieService } from '../../services/movie.service';
 import { RatingService } from '../../services/rating.service';
 import { IMovie } from '../../models/movie.model';
 import { IRating } from '../../models/rating.model';
+
 
 @Component({
   selector: 'app-movie-detail',
@@ -28,15 +29,23 @@ export class MovieDetailComponent implements OnInit {
   rated:IRating;
   id:number;
   paramId:number=+this.route.snapshot.params['id']
-  
+
 
   // assigning my movieService from movie.service.ts etc.
   // same with route, taking from @angular/router/
   constructor(private movieService: MovieService,
               private ratingService: RatingService,
-              private route:ActivatedRoute) {
+              private route:ActivatedRoute,
+              private router: Router) {
   }
 
+  getRating():void {
+    this.ratingService.getStar(this.paramId, 5).subscribe(rating => this.value5 = rating);
+    this.ratingService.getStar(this.paramId, 4).subscribe(rating => this.value4 = rating);
+    this.ratingService.getStar(this.paramId, 3).subscribe(rating => this.value3 = rating);
+    this.ratingService.getStar(this.paramId, 2).subscribe(rating => this.value2 = rating);
+    this.ratingService.getStar(this.paramId, 1).subscribe(rating => this.value1 = rating);
+  }
   // + makes a number from String, getMovie() needs a number :id
   // i'm assigning this.movie to be one from getMovie() method.
   // getMovie(id) needs movie.id, when i click on card this route.snapshot.params "knows" which one got what id.
@@ -46,49 +55,50 @@ export class MovieDetailComponent implements OnInit {
     this.movieService.getMovie(this.paramId).subscribe(movie => this.movie = movie);
 
   //getMovieRating() used 'id' from route.snapshot.params.
-    console.log(this.ratingService.getMovieRating(this.paramId).subscribe(rating => this.rating = rating));
-    console.log(this.rating);
+  //I DONT NEED THIS ANY MORE
+    this.ratingService.getMovieRating(this.paramId).subscribe((rating) => this.rating = rating);
+    
+    this.getRating();
+  
   //My value is binded.
   //Don't wanna make 5 values for each progress. Gonna figure it out somehow.
   //I shouldn't access DOM and put values from DOM to Angular. It's other way around.
   //And i shouldn't use ElementRef, so i guess i'll just make 5 values for now.
   //I could try loop but no idea how to implement my values to diff progress bars. this.(value+{{n}}) = this.ratingService.getStar(this.rating, {{n}})? sth like that.
-    this.ratingService.getStar(this.paramId, 5).subscribe(rating => this.value5 = rating);
-    this.ratingService.getStar(this.paramId, 4).subscribe(rating => this.value4 = rating);
-    this.ratingService.getStar(this.paramId, 3).subscribe(rating => this.value3 = rating);
-    this.ratingService.getStar(this.paramId, 2).subscribe(rating => this.value2 = rating);
-    this.ratingService.getStar(this.paramId, 1).subscribe(rating => this.value1 = rating);
+    
     // this.value4 = this.ratingService.getStar(this.rating, 4);
     // this.value3 = this.ratingService.getStar(this.rating, 3);
     // this.value2 = this.ratingService.getStar(this.rating, 2);
     // this.value1 = this.ratingService.getStar(this.rating, 1);
-
-  //maxValue will be highest of those 5.
-    this.maxValue = Math.max(+this.value1,
-                             +this.value2,
-                             +this.value3,
-                             +this.value4,
-                             +this.value5).toString();
-    console.log(this.maxValue);
-  //averageRating gives 2 decimal places.
-    // this.averageRating = this.ratingService.getAverageRating(this.rating);
   }
 
-  
-  //Ok i'm gonna try to get values here. I need IRating model to receive but for now I'll get just value.
-  // getRating(formValues) {
-  //   //taking highest id from array +1
-  //   this.id = this.ratingService.getHighestId(this.rating)+1;
-  //   //getting :IRating with diff id, movie_id param and our formValue.
-  //   this.rated = {
-  //     "id":this.id,
-  //     "movie_id":+this.route.snapshot.params['id'],
-  //     "rating":formValues.rating
-  //   }
-  //   //i Guess that's enough for now. I need to push it anyway with http.
-  //   console.log(this.rating = this.rating.concat(this.rated));
-  // }
+    //OLD CODE
+  //maxValue will be highest of those 5.
+    // this.maxValue = Math.max(+this.value1,
+    //                          +this.value2,
+    //                          +this.value3,
+    //                          +this.value4,
+    //                          +this.value5).toString();
+    // console.log(this.maxValue);
+  //averageRating gives 2 decimal places.
+    // this.averageRating = this.ratingService.getAverageRating(this.rating);
 
+    saveRating(formValues){
+      this.ratingService.saveRating(this.paramId, formValues).subscribe(rating => {
+        console.log(rating)
+        this.getRating()});
+    }
 
+      //     console.log(this.rated = {
+      // "id":this.id,
+      // "movie_id":+this.route.snapshot.params['id'],
+      // "rating":formValues.rating
+
+    // //taking highest id from array +1
+    // this.id = this.ratingService.getHighestId(this.rating)+1;
+    // //getting :IRating with diff id, movie_id param and our formValue.
+
+    // //i Guess that's enough for now. I need to push it anyway with http.
+    // console.log(this.rating = this.rating.concat(this.rated));
 }
 
--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs';
 import { IRating } from '../models/rating.model';
 import 'rxjs';
@@ -15,7 +15,7 @@ export class RatingService {
    // Received filtered array with array.prototype.filter method. I'm gonna filter movie_id attribute with id path of movies.
    getMovieRating(id:number):Observable<IRating[]> {
      return this.http.get("https://movie-ranking.herokuapp.com/movies/"+id.toString()+"/ratings")
-                     .map((res:Response) => res.json());
+                     .map((res:Response) => <IRating[]>res.json());
    }
 
    //used my filtered movie array, and used the star (1-5) parameter.
@@ -28,6 +28,7 @@ export class RatingService {
                       .map(rating => rating.filter((rating)=>rating.rating == star).length.toString())
                       
    }
+    
         // return this.getMovieRating().reduce(
         // function(n, ratings){
         //   return n + +(ratings.rating == star);
@@ -41,104 +42,114 @@ export class RatingService {
   //    return avrg.toString();
   //  }
 
-   //taking highest id from array. Math.max.apply(Math,array). And then i use map to only get id from whole rating.
+  //I DONT NEED THIS
+   // taking highest id from array. Math.max.apply(Math,array). And then i use map to only get id from whole rating.
   //  getHighestId(rating:IRating[]) {
   //    return Math.max.apply(Math,
   //       rating.map(function(attr){
   //          return attr.id}))
   //  }
+
+   saveRating(id:number, formValues):Observable<IRating> {
+    let headers = new Headers({ 'Content-Type':'application/json'});
+    let options = new RequestOptions({headers:headers});
+
+  
+    return this.http.post("https://movie-ranking.herokuapp.com/movies/" + id + "/ratings", JSON.stringify(formValues), options)
+                    .map((res:Response) => { return res.json() });
+  }
 }
 
 //Hardcoded Rating with model style.
-const RATING:IRating[] = [
-  {
-    "id": 274,
-    "movie_id": 12,
-    "rating": 5
-  },
-  {
-    "id": 280,
-    "movie_id": 12,
-    "rating": 1
-  },
-  {
-    "id": 282,
-    "movie_id": 12,
-    "rating": 5
-  },
-  {
-    "id": 284,
-    "movie_id": 11,
-    "rating": 1
-  },
-  {
-    "id": 285,
-    "movie_id": 12,
-    "rating": 1
-  },
-  {
-    "id": 286,
-    "movie_id": 12,
-    "rating": 1
-  },
-  {
-    "id": 292,
-    "movie_id": 13,
-    "rating": 4
-  },
-  {
-    "id": 297,
-    "movie_id": 13,
-    "rating": 2
-  },
-  {
-    "id": 298,
-    "movie_id": 12,
-    "rating": 3
-  },
-    {
-    "id": 274,
-    "movie_id": 10,
-    "rating": 5
-  },
-  {
-    "id": 280,
-    "movie_id": 10,
-    "rating": 1
-  },
-  {
-    "id": 282,
-    "movie_id": 11,
-    "rating": 1
-  },
-  {
-    "id": 284,
-    "movie_id": 11,
-    "rating": 1
-  },
-  {
-    "id": 285,
-    "movie_id": 12,
-    "rating": 1
-  },
-  {
-    "id": 286,
-    "movie_id": 12,
-    "rating": 1
-  },
-  {
-    "id": 292,
-    "movie_id": 13,
-    "rating": 4
-  },
-  {
-    "id": 297,
-    "movie_id": 13,
-    "rating": 2
-  },
-  {
-    "id": 298,
-    "movie_id": 12,
-    "rating": 3
-  }
-]
+// const RATING:IRating[] = [
+//   {
+//     "id": 274,
+//     "movie_id": 12,
+//     "rating": 5
+//   },
+//   {
+//     "id": 280,
+//     "movie_id": 12,
+//     "rating": 1
+//   },
+//   {
+//     "id": 282,
+//     "movie_id": 12,
+//     "rating": 5
+//   },
+//   {
+//     "id": 284,
+//     "movie_id": 11,
+//     "rating": 1
+//   },
+//   {
+//     "id": 285,
+//     "movie_id": 12,
+//     "rating": 1
+//   },
+//   {
+//     "id": 286,
+//     "movie_id": 12,
+//     "rating": 1
+//   },
+//   {
+//     "id": 292,
+//     "movie_id": 13,
+//     "rating": 4
+//   },
+//   {
+//     "id": 297,
+//     "movie_id": 13,
+//     "rating": 2
+//   },
+//   {
+//     "id": 298,
+//     "movie_id": 12,
+//     "rating": 3
+//   },
+//     {
+//     "id": 274,
+//     "movie_id": 10,
+//     "rating": 5
+//   },
+//   {
+//     "id": 280,
+//     "movie_id": 10,
+//     "rating": 1
+//   },
+//   {
+//     "id": 282,
+//     "movie_id": 11,
+//     "rating": 1
+//   },
+//   {
+//     "id": 284,
+//     "movie_id": 11,
+//     "rating": 1
+//   },
+//   {
+//     "id": 285,
+//     "movie_id": 12,
+//     "rating": 1
+//   },
+//   {
+//     "id": 286,
+//     "movie_id": 12,
+//     "rating": 1
+//   },
+//   {
+//     "id": 292,
+//     "movie_id": 13,
+//     "rating": 4
+//   },
+//   {
+//     "id": 297,
+//     "movie_id": 13,
+//     "rating": 2
+//   },
+//   {
+//     "id": 298,
+//     "movie_id": 12,
+//     "rating": 3
+//   }
+// ]
